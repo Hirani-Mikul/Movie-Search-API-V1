45ae4f598b3a245804a420af12686f537cb4e187
Mikul Hirani <hiranimikul71@gmail.com>
2022-01-01 17:51:26 +0300

    final commit

---
+++ src/Form.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+import MovieCard from "./MovieCard";
+import Loader from "./Loader";
+
+export default function Form() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovie] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const API_KEY = 'YOUR API KEY';
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setLoading(false);
+      setMovie(data.results);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  function handleChange(e) {
+    setQuery(e.target.value);
+  }
+  return (
+    <div>
+      <form onSubmit={searchMovie}>
+        <label htmlFor="query">Search Movie: </label>
+        <input
+          type="text"
+          name="query"
+          value={query}
+          onChange={handleChange}
+          placeholder="Type Movie Here"
+        />
+        <button className="btn" type="submit">
+          Search
+        </button>
+      </form>
+      <MovieCard data={movies} />
+      {isLoading ? <Loader /> : null}
+    </div>
+  );
+}
+/*
+  DISPLAY 9 MOVIES AT START UP
+  WHEN YOU CLICK IT TAKES YOU TO THE PAGE
+*/
